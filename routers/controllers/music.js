34754556b54d72d11dc58aaf163fb6922fdaa057
ihesah1 <Hesah.Shaman.elq.js@tuwaiq.edu.sa>
2021-11-13 00:51:57 +0300

    add music search

--- routers/controllers/music.js
+++ routers/controllers/music.js
@@ -1,7 +1,17 @@
 const axios = require('axios');
 
-const getAllMusicWithQuery = (req, res) => {
-  // code
+const getAllMusicWithQuery = async(req, res) => {
+ 
+  const searchInput = req.query.search;
+    if (searchInput) {
+     const response = await axios.get(`http://itunes.apple.com/search?term=${searchInput}&media=music&limit=50`)
+      res.status(200);
+      res.json(response.data.results);
+    } else {
+      const response = await axios.get(`http://itunes.apple.com/search?term=all&media=music&limit=50`)
+      res.status(200);
+      res.json(response.data.results);
+    }
 };
 
 module.exports = getAllMusicWithQuery;
