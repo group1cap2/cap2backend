0d206488de7f72719551b2197d9d5ea0614c4902
ihesah1 <Hesah.Shaman.elq.js@tuwaiq.edu.sa>
2021-11-12 22:20:51 +0300

    add search ebooks

--- routers/controllers/books.js
+++ routers/controllers/books.js
@@ -1,5 +1,14 @@
-const getAllBooksWithQuery = (req, res) => {
-  // code
+const getAllBooksWithQuery = async(req, res) => {
+  const searchInput = req.query.search;
+  if (searchInput) {
+   const response = await axios.get(`http://itunes.apple.com/search?term=${searchInput}&media=ebook&limit=50`)
+    res.status(200);
+    res.json(response.data.results);
+  } else {
+    const response = await axios.get(`http://itunes.apple.com/search?term=all&media=ebook&limit=5`)
+    res.status(200);
+    res.json(response.data.results);
+  }
 };
 
 module.exports = getAllBooksWithQuery;
