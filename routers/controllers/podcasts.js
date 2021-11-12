8dad87bfb7e319410fee40c0ef002edf3fd80e86
ihesah1 <Hesah.Shaman.elq.js@tuwaiq.edu.sa>
2021-11-13 00:55:51 +0300

    add podcast search

--- routers/controllers/podcasts.js
+++ routers/controllers/podcasts.js
@@ -1,7 +1,16 @@
 const axios = require('axios');
 
-const getAllPodcastsWithQuery = (req, res) => {
-  // code
+const getAllPodcastsWithQuery = async(req, res) => {
+  const searchInput = req.query.search;
+  if (searchInput) {
+   const response = await axios.get(`http://itunes.apple.com/search?term=${searchInput}&media=podcast&limit=50`)
+    res.status(200);
+    res.json(response.data.results);
+  } else {
+    const response = await axios.get(`http://itunes.apple.com/search?term=all&media=podcast&limit=50`)
+    res.status(200);
+    res.json(response.data.results);
+  }
 };
 
 module.exports = getAllPodcastsWithQuery;
