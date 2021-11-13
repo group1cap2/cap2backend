0367d2fd984686dc6b83c75f71d62f5d701f028d
ibrahimalsaif-tuwaiq <Ibrahim.Husain.elq.js@tuwaiq.edu.sa>
2021-11-14 01:53:42 +0300

    update readme and fix styling

--- routers/controllers/podcasts.js
+++ routers/controllers/podcasts.js
@@ -1,16 +1,19 @@
-const axios = require('axios');
+const axios = require("axios");
 
-const getAllPodcastsWithQuery = async(req, res) => {
-
+const getAllPodcastsWithQuery = async (req, res) => {
   const searchInput = req.query.search;
   const limit = req.query.limit;
 
   if (searchInput) {
-   const response = await axios.get(`http://itunes.apple.com/search?term=${searchInput}&media=podcast&limit=${limit}`)
+    const response = await axios.get(
+      `http://itunes.apple.com/search?term=${searchInput}&media=podcast&limit=${limit}`
+    );
     res.status(200);
     res.json(response.data.results);
   } else {
-    const response = await axios.get(`http://itunes.apple.com/search?term=all&media=podcast&limit=${limit}`)
+    const response = await axios.get(
+      `http://itunes.apple.com/search?term=all&media=podcast&limit=${limit}`
+    );
     res.status(200);
     res.json(response.data.results);
   }
