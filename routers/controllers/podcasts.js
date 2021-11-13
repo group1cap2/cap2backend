ec51f27bd5fd95fdf5ace6e6bd99898739888974
ibrahimalsaif-tuwaiq <Ibrahim.Husain.elq.js@tuwaiq.edu.sa>
2021-11-13 14:34:12 +0300

    adding limit to endpoints

--- routers/controllers/podcasts.js
+++ routers/controllers/podcasts.js
@@ -1,13 +1,16 @@
 const axios = require('axios');
 
 const getAllPodcastsWithQuery = async(req, res) => {
+
   const searchInput = req.query.search;
+  const limit = req.query.limit;
+
   if (searchInput) {
-   const response = await axios.get(`http://itunes.apple.com/search?term=${searchInput}&media=podcast&limit=50`)
+   const response = await axios.get(`http://itunes.apple.com/search?term=${searchInput}&media=podcast&limit=${limit}`)
     res.status(200);
     res.json(response.data.results);
   } else {
-    const response = await axios.get(`http://itunes.apple.com/search?term=all&media=podcast&limit=50`)
+    const response = await axios.get(`http://itunes.apple.com/search?term=all&media=podcast&limit=${limit}`)
     res.status(200);
     res.json(response.data.results);
   }
